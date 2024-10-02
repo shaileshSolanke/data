af20722f2dc4b3557d608058ae33848ae28b1a69
shaileshSolanke <shaileshsolanke512@gmail.com>
2024-10-02 16:08:51 +0530

    new data added

---
+++ react-mini-projects-data/01-accordian/cosmic-calendar.js
@@ -0,0 +1,349 @@
+const cosmicCalendar = [
+  {
+    id: 1,
+    month: "January",
+    events: [
+      {
+        day: "01",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jan01.svg",
+            timeStamp: [0, 0, 0, 0],
+            description: "Big Bang : space and time are created",
+          },
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jan02.svg",
+            timeStamp: [1, 2, 1, 4],
+            description: "First atoms form",
+          },
+        ],
+      },
+      {
+        day: "03",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jan03.svg",
+            timeStamp: [false],
+            description: "First stars form",
+          },
+        ],
+      },
+      {
+        day: "20",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jan04.svg",
+            timeStamp: [false],
+            description: "Large galaxies form",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    month: "February",
+    events: [
+      {
+        day: "15",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/feb01.svg",
+            timeStamp: [false],
+            description: "Universe's large-scale structure begins forming",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    month: "March",
+    events: [
+      {
+        day: "15",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/mar01.svg",
+            timeStamp: [false],
+            description: "Milky Way forms",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    month: "April",
+    events: [
+      {
+        day: "1",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/apr01.svg",
+            timeStamp: [false],
+            description: "First galaxy clusters form",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    month: "May",
+    events: [
+      {
+        day: "5",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/may01.svg",
+            timeStamp: [false],
+            description: "Earliest Sun-like stars appear",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    month: "June",
+    events: [
+      {
+        day: "5",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jun01.svg",
+            timeStamp: [false],
+            description: "Great walls and filaments take shape",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    month: "July",
+    events: [
+      {
+        day: "30",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/jul01.svg",
+            timeStamp: [false],
+            description: "Alpha Centauri forms",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    month: "August",
+    events: [
+      {
+        day: "5",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/aug01.svg",
+            timeStamp: [false],
+            description: "Milky Way becomes a spiral galaxy",
+          },
+        ],
+      },
+      {
+        day: "15",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/aug02.svg",
+            timeStamp: [false],
+            description: "Dark-energy-dominated era begins",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 9,
+    month: "September",
+    events: [
+      {
+        day: "1",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/sep01.svg",
+            timeStamp: [false],
+            description: "Solar system forms",
+          },
+        ],
+      },
+      {
+        day: "10",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/sep02.svg",
+            timeStamp: [false],
+            description: "Life emerges on Earth",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 10,
+    month: "October",
+    events: [
+      {
+        day: "1",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/oct01.svg",
+            timeStamp: [false],
+            description: "Photosynthesis originates",
+          },
+        ],
+      },
+      {
+        day: "29",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/oct01.svg",
+            timeStamp: [false],
+            description: "Oxygen appears in Earth's atmosphere",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 11,
+    month: "November",
+    events: [
+      {
+        day: "1",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/nov01.svg",
+            timeStamp: [false],
+            description: "Ozone layer forms",
+          },
+        ],
+      },
+      {
+        day: "8",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/nov02.svg",
+            timeStamp: [false],
+            description: "Eukaryotic organisms emerge",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 12,
+    month: "December",
+    events: [
+      {
+        day: "5",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec01.svg",
+            timeStamp: [false],
+            description: "Multicellular organisms appear",
+          },
+        ],
+      },
+      {
+        day: "19",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec02.svg",
+            timeStamp: [false],
+            description: "Life comes out of the water",
+          },
+        ],
+      },
+      {
+        day: "25",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec03.svg",
+            timeStamp: [false],
+            description: "Dinosaurs appear",
+          },
+        ],
+      },
+      {
+        day: "26",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec04.svg",
+            timeStamp: [false],
+            description: "First mammals appear",
+          },
+        ],
+      },
+      {
+        day: "30",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec05.svg",
+            timeStamp: [false],
+            description: "Dinosaurs go extinct",
+          },
+        ],
+      },
+      {
+        day: "31",
+        event: [
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec06.svg",
+            timeStamp: [1, 0, 1, 5],
+            description: "Apes appear",
+          },
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec07.svg",
+            timeStamp: [2, 0, 0, 0],
+            description: "Humans split from apes",
+          },
+          {
+            img: "https://shaileshsolanke.github.io/react-mini-projects-data/01-accordian/assets/dec08.svg",
+            timeStamp: [2, 3, 5, 3],
+            description: "Homo Sapiens appear",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const bonus = {
+  title: "last seconds of December 31",
+  events: [
+    {
+      timeLeft: [1, 3],
+      event: "Wheel is invented",
+    },
+    {
+      timeLeft: [1, 2],
+      event: "Writing is invented",
+    },
+    {
+      timeLeft: [1, 0],
+      event: "Egyption pyramids are built",
+    },
+    {
+      timeLeft: [0, 2],
+      event: "Galileo uses his telescope",
+    },
+    {
+      timeLeft: [0, 1],
+      event: "Humans land on the Moon",
+    },
+    {
+      timeLeft: [0, 0],
+      event: "You reading this...😁",
+    },
+  ],
+};
